699a2d865b6a2941777603193de4a9b80819857e
ms <maemotosato@gmail.coms>
2024-08-06 22:42:29 +0900

    test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { act } from 'react';
 
 test('renders learn react link', () => {
-  render(<App />);
+  act(() => {
+    render(<App />);
+  });
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
